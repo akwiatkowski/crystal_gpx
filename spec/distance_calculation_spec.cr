8c2ae5b3c8deca471b42024e905e91c95a97b58a
Aleksander Kwiatkowski <bobikx@poczta.fm>
2016-12-25 17:02:11 +0100

    distance calculation

---
+++ spec/distance_calculation_spec.cr
@@ -0,0 +1,68 @@
+require "./spec_helper"
+
+describe CrystalGpx::Point do
+  it "create Point with geo coords" do
+    lat = 52.40285
+    lon = 16.91062
+    o = CrystalGpx::Point.new(lat, lon)
+
+    o.lat.should eq lat
+    o.lon.should eq lon
+  end
+
+  it "calculate distance Poznan-Watsaw without using Point struct" do
+    poznan_lat = 52.40285
+    poznan_lon = 16.91062
+
+    warsaw_lat = 52.23044
+    warsaw_lon = 21.00458
+
+    # named params
+    d = CrystalGpx::Point.distance(
+      lat1: poznan_lat,
+      lon1: poznan_lon,
+      lat2: warsaw_lat,
+      lon2: warsaw_lon
+    )
+
+    d.should be > 278.70
+    d.should be < 278.90
+
+    # regular params
+    d = CrystalGpx::Point.distance(
+      poznan_lat,
+      poznan_lon,
+      warsaw_lat,
+      warsaw_lon
+    )
+
+    d.should be > 278.70
+    d.should be < 278.90
+  end
+
+  it "calculate distance Poznan-Watsaw without using Point struct" do
+    poznan_lat = 52.40285
+    poznan_lon = 16.91062
+
+    warsaw_lat = 52.23044
+    warsaw_lon = 21.00458
+
+    poznan = CrystalGpx::Point.new(lat: poznan_lat, lon: poznan_lon)
+    warsaw = CrystalGpx::Point.new(lat: warsaw_lat, lon: warsaw_lon)
+
+    # class method
+    d = CrystalGpx::Point.distance(poznan, warsaw)
+
+    d.should be > 278.70
+    d.should be < 278.90
+
+    # instance method
+    d = poznan.distance_to(warsaw)
+
+    d.should be > 278.70
+    d.should be < 278.90
+
+  end
+
+
+end
--- src/crystal_gpx/point.cr
+++ src/crystal_gpx/point.cr
@@ -1,12 +1,17 @@
 require "xml"
 
 struct CrystalGpx::Point
+  D2R = Math::PI / 180.0
+
   @lat : Float64 = 0.0
   @lon : Float64 = 0.0
   @ele : Float64 = 0.0
   @time : Time = Time.now
 
   property :lat, :lon, :ele, :time
+
+  def initialize(@lat : Float64, @lon : Float64)
+  end
 
   def self.from_node(n : XML::Node, time_type = Time::Kind::Local)
     return nil if n["lat"]?.nil? || n["lon"]?.nil?
@@ -59,5 +64,29 @@ struct CrystalGpx::Point
     point.ele += (after.ele - before.ele) * (time - before.time).to_f / (after.time - before.time).to_f
 
     return point
+  end
+
+  def self.distance(lat1, lon1, lat2, lon2)
+    # http://stackoverflow.com/questions/365826/calculate-distance-between-2-gps-coordinates
+    dlong = (lon2 - lon1) * D2R
+    dlat = (lat2 - lat1) * D2R
+    a = (Math.sin(dlat / 2.0) ** 2.0) + Math.cos(lat1 * D2R) * Math.cos(lat2 * D2R) * (Math.sin(dlong / 2.0) ** 2.0)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt( 1.0 - a))
+    d = 6367.0 * c;
+
+    return d
+  end
+
+  def self.distance(point1 : CrystalGpx::Point, point2 : CrystalGpx::Point)
+    return distance(
+      lat1: point1.lat,
+      lon1: point1.lon,
+      lat2: point2.lat,
+      lon2: point2.lon
+    )
+  end
+
+  def distance_to(other_point : CrystalGpx::Point)
+    return self.class.distance(point1: self, point2: other_point)
   end
 end
